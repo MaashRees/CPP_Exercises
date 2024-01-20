417c6bef745eea966975090c39955c90a5742bb8
Seer MENSAH ASSIAKOLEY <seerharley03@gmail.com>
2024-01-20 17:41:34 +0100

    tp1 -ex2 -q4

---
+++ tp1/ex2/Person.cpp
@@ -0,0 +1,11 @@
+#include "Person.hpp"
+#include <string>
+
+    
+
+std::string Person::get_first_name(){
+    return _first_name;
+}
+std::string Person::get_surname(){
+    return _surname;
+}
--- tp1/ex2/Person.hpp
+++ tp1/ex2/Person.hpp
@@ -3,6 +3,14 @@
 class Person
 {
 public:
-    std::string first_name;
-    std::string surname;
+    Person(std::string f_name, std::string surname)
+    : _first_name { f_name},
+    _surname{surname}
+    {};
+    
+    std::string get_first_name();
+    std::string get_surname();
+private:
+    std::string _first_name;
+    std::string _surname;
 };
