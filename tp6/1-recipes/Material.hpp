dd3cda0cb31b4e01ec81e71a6733115219d9d9a0
Seer MENSAH ASSIAKOLEY <98156140+MaashRees@users.noreply.github.com>
2024-02-29 12:45:03 +0100

    TP6 B- Materiaux C - Recettes (bonus not finished)

--- tp6/1-recipes/Material.hpp
+++ tp6/1-recipes/Material.hpp
@@ -1,9 +1,38 @@
 #pragma once
 
 #include <ostream>
+#include <string>
+#include <iostream>
 
 class Material
 {
+public :
+    /* Other method constructor
+    Material(const std::string& name)
+    :_name{name}
+    {
+        std::cout << name << " was created" << std::endl;
+    }*/
+
+    /*Mieux*/
+    Material(std::string name)
+    :_name{std::move(name)}
+    {
+        std::cout << _name << " was created" << std::endl;
+    }
+
+    ~Material(){
+        std::cout << _name << " was destroyed" << std::endl;
+    }
     // Affiche le nom d'un materiau.
-    friend std::ostream& operator<<(std::ostream& stream, const Material& material) { return stream; }
+    friend std::ostream& operator<<(std::ostream& stream, const Material& material) { 
+        stream << material._name;
+        return stream; 
+    }
+    std::string get_name() const{
+        return _name;
+    }
+private:
+        std::string _name;
+
 };
--- tp6/1-recipes/ProgramData.cpp
+++ tp6/1-recipes/ProgramData.cpp
@@ -2,16 +2,59 @@
 
 
 void ProgramData::add_material(std::string name)
-{}
+{
+    _inventaire.push_back(std::make_unique<Material>(std::move(name)));
+
+}
 
 void ProgramData::get_materials(std::vector<const Material*>& materials) const
-{}
+{
+    for(auto index = 0; index < _inventaire.size(); index++){
+
+        auto m = _inventaire[index].get();
+        materials.push_back(m);
+    }
+}
 
 void ProgramData::register_recipe(std::vector<std::string> materials, std::vector<std::string> products)
-{}
+{
+    auto size = _recipes.size();
+    auto id = size + 1;
+    auto recipe = std::make_unique<Recipe>(std::move(materials), std::move(products), id); 
+    _recipes.push_back(std::move(recipe));
+    std::cout <<"Recipe "<< id << " was been registered" << std::endl;
+}
 
+bool inInventaire(std::string name, std::vector<const Material*> inventaire){
+    for (auto m : inventaire){
+        if(m->get_name() == name){
+            return true;
+        }
+    }
+    return false;
+}
 void ProgramData::collect_doable_recipes(std::vector<const Recipe*>& recipes) const
-{}
+{   
+    auto inventaire = std::vector<const Material*> {};
+    for(auto index = 0; index < _recipes.size(); index++){
+
+        auto r = _recipes[index].get();
+        bool add= true;
+        auto materials = r->get_m();
+        
+        get_materials(inventaire);
+        for(auto m : materials){
+            if(inInventaire(m, inventaire) == false){
+                add = false;
+                break;
+            }
+        }
+        if(add){
+
+            recipes.push_back(r);
+        }
+    }
+}
 
 ProductionResult ProgramData::produce(size_t recipe_id)
 {
